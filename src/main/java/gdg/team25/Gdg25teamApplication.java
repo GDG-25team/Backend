650f78fd5733a9458885a195aca3c4957325dce1
gaeul3303 <rkdmfghd@naver.com>
2025-01-18 16:09:34 +0900

    init project

---
+++ src/main/java/gdg/team25/Gdg25teamApplication.java
@@ -0,0 +1,13 @@
+package gdg.team25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gdg25teamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gdg25teamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gdg/team25/Gdg25teamApplicationTests.java
@@ -0,0 +1,13 @@
+package gdg.team25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gdg25teamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
